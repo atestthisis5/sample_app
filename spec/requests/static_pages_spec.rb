7cdb58db82608788756c5ab9e39c2dd51db67c85
atestthisis5 <atestthisis5@gmail.com>
2013-10-27 19:17:30 -0400

    Add RSpec helper function ; clean up tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,13 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
-  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
-  
   subject { page }
 
   describe "Home Page" do
     before { visit root_path }
 
     it { should have_selector('h1', text: 'Sample App') }
-
-    it { page.should have_selector('title', text: "#{base_title}") }
-
+    it { page.should have_selector('title', text: full_title) }
     it { page.should_not have_selector('title', text: "|") }
   end
 
@@ -20,24 +16,21 @@ describe "Static Pages" do
     before { visit help_path }
 
     it { should have_selector('h1', text: 'Help') }
-
-    it { page.should have_selector('title', text: "#{base_title} | Help") }
+    it { page.should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About Page" do
     before { visit about_path }
 
     it { should have_selector('h1', text: 'About Us') }
-
-    it { page.should have_selector('title', text: "#{base_title} | About Us") }
+    it { page.should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact Page" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }
-
-    it { page.should have_selector('title', text: "#{base_title} | Contact") }
+    it { page.should have_selector('title', text: full_title('Contact')) }
   end
 
 end
