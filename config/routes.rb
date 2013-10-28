e7ebea32001a69634b1d78a27ab3d391d98978e5
atestthisis5 <atestthisis5@gmail.com>
2013-10-27 20:57:48 -0400

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ SampleApp::Application.routes.draw do
   
 
   # static pages
-  match '/help', to: ' static_pages#help'
-  match '/about', to: ' static_pages#about'
-  match '/contact', to: ' static_pages#contact'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
 
   # The priority is based upon order of creation:
