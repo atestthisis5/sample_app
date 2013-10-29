b71b1f6995fa93daf6c980f0f65f75a7382532ec
atestthisis5 <atestthisis5@gmail.com>
2013-10-28 23:03:22 -0400

    Handle signup failure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
 end
