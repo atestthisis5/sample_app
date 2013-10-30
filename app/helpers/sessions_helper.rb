80132ca21a47d645c9b56f47a229e928637e7b04
atestthisis5 <atestthisis5@gmail.com>
2013-10-29 21:49:27 -0400

    Add signin, setter, and getter methods for current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+      # this is the same thing as:
+      #   cookies[:remember_token] = { value:   user.remember_token,
+      #                        expires: 20.years.from_now.utc }
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
 end
