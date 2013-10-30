307d65da3c1f7d4019e1450f9bd3c6edd733badc
atestthisis5 <atestthisis5@gmail.com>
2013-10-29 21:15:46 -0400

    Plant a CSRF safeguard (sign_out not defined yet)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
