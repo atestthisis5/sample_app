ee00fca92e4c08ec2d125e763ed4e949bcfaeff5
atestthisis5 <atestthisis5@gmail.com>
2013-11-02 17:24:37 -0400

    Add pagination to User List function

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,10 @@
 
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    # name     "Michael Hartl"
+    sequence(:name)  { |n| "Person #{n}" }
+    # email    "michael@example.com"
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
   end
